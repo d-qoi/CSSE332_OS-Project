0ddfe1e2c1a7a83d566941f6e3947b53b2842d26
David Mehl <mehlda@rose-hulman.edu>
2017-04-14 03:26:56 -0400

    finished shell script and kernel.c file, still need to add names

--- m1/compileOS.sh
+++ m1/compileOS.sh
@@ -8,3 +8,12 @@ echo Finished creaing empty floppya.img
 echo Copying bootloader to floppya.img
 dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
 echo Completed Milestone 1 Part 4
+echo Starting Milestone 1 Part 6
+bcc -ansi -c -o kernel.o kernel.c
+as86 kernel.asm -o kernel_asm.o
+ld86 -o kernel -d kernel.o kernel_asm.o
+echo Completed compiling, assembling, and linking
+echo Starting to copy to floppya.img
+dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
+echo Done
+
