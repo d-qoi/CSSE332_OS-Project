7e2cdff602786b1a94791d23f11c13fddeca5c02
David Mehl <mehlda@rose-hulman.edu>
2017-04-25 13:40:23 -0400

    formatted fs.c

--- m3/fs.c
+++ m3/fs.c
@@ -7,28 +7,28 @@
  #include "interrupt.h"
  
  void readFile(char * fname, char * buffer) {
-	char dir[512];
-	int i, j, k, equal;
-	
-	readSector(&dir, 2);
-	
-	for (i = 0; i < 16; i++) {
-		if (dir[i*32] == 0x0)
-			continue;
-		equal = 1;
-		for (j = 0; j < 6; j++) {
-			if (dir[i*32+j] != fname[j]) {
-				equal = 0;
-				break;
-			}
-		}
-		if (equal){
-			for (k = 0; k < 26; k++) {
-				if (dir[i*32 + 6 + k] == 0x0)
-					break;
-				readSector(buffer + 512*k, dir[i*32 + 6 + k]);
-			}
-			return;
-		}
-	}
+   char dir[512];
+   int i, j, k, equal;
+   
+   readSector(&dir, 2);
+   
+   for (i = 0; i < 16; i++) {
+     if (dir[i*32] == 0x0)
+       continue;
+     equal = 1;
+     for (j = 0; j < 6; j++) {
+       if (dir[i*32+j] != fname[j]) {
+	 equal = 0;
+	 break;
+       }
+     }
+     if (equal){
+       for (k = 0; k < 26; k++) {
+	 if (dir[i*32 + 6 + k] == 0x0)
+	   break;
+	 readSector(buffer + 512*k, dir[i*32 + 6 + k]);
+       }
+       return;
+     }
+   }
  }
