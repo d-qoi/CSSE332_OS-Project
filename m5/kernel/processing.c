9f1ce2d0e79a094d3f23694f1ece54d7b3e6b1f4
Alex Hirschfeld <alex@d-qoi.com>
2017-05-06 14:13:01 -0400

    processing.* compiles, adding to interrupt next

---
+++ m5/kernel/processing.c
@@ -0,0 +1,63 @@
+/*
+  Milestone 3 kernel/interrupt.c
+  Group 3-C
+  David Mehl, Christian Balcom, and Alexander Hirschfeld
+ */
+
+#include "processing.h"
+#include "lib/string.h"
+
+/* allocates a process if no segment is given */
+struct process *allocateProcess() {
+  int i = 0;
+  for (i = 0; i < PROCESSLIMIT; i++) {
+    if (processTable[i].segment == 0) {
+      processTable[i].segment == TOSEGMENT(i + 2);
+      return &processTable[i];
+    }
+  }
+  return 0;
+}
+
+/*
+returns the process struct that works with the segment
+or returns a new process struct
+ */
+struct process *reallocateProcess(int segment) {
+  segment = TOINT(segment);
+  if (((segment - 2) >= 0) && ((segment - 2) <= 32)) {
+    processTable[segment - 2].segment = TOSEGMENT(segment);
+    return &processTable[segment - 2];
+  }
+  return allocateProcess();
+}
+
+/* zeroes out a process struct at a given segment */
+struct process *freeProcess(int segment) {
+  segment = TOINT(segment);
+  if (((segment - 2) >= 0) && ((segment - 2) <= 32)) {
+    memset(processTable[segment - 2], 0, sizeof(struct process));
+    return &processTable[segment - 2];
+  }
+  return 0;
+}
+
+struct process *getProcessByName(char *name) {
+  int i;
+  for (i = 0; i < PROCESSLIMIT; i++)
+    if (strcmp(name, processTable[i].name))
+      return &processTable[i];
+  return 0;
+}
+
+struct process *getCurrentProcess() {
+  int i;
+  for (i = 0; i < PROCESSLIMIT; i++)
+    if (processTable[i].running)
+      return &processTable[i];
+
+  /* if it is unable to find a running process */
+  processTable[0].running = 1;
+  return &processTable[0];
+}
+
---
+++ m5/kernel/processing.h
@@ -0,0 +1,32 @@
+/*
+  kernel/processing.h
+  Group 3-C
+  David Mehl, Christian Balcom, and Alexander Hirschfeld
+ */
+
+
+#define PROCESSNAMELIMIT 15
+#define PROCESSLIMIT 32
+
+#define TOSEGMENT(I) I<<12;	/* segment is 0xN000 */
+#define TOINT(S) S>>12;		
+
+/*
+Definitions of interrupt commands
+*/
+
+struct process* allocateProcess();
+struct process* reallocateProcess(int segment);
+struct process* freeProcess(int segment);
+struct process* getProcessByName(char * name);
+struct process* getCurrentProcess();
+
+struct process {
+	int segment;
+	char running;
+	char name[PROCESSNAMELIMIT];
+};
+
+struct process processTable[PROCESSLIMIT];
+
+
