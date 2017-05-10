424c84a95cf340152f0b6771d6d0cd8a1071198c
Alex Hirschfeld <alex@d-qoi.com>
2017-05-10 03:44:21 -0400

    More files

--- m5/userspace/src/cp.c
+++ m5/userspace/src/cp.c
@@ -1,5 +1,5 @@
 /*
-  userspace/src/cat.c
+  userspace/src/cp.c
   Group 3-C
   David Mehl, Christian Balcom, and Alexander Hirschfeld
 */
@@ -13,24 +13,56 @@ int main() {
   char *arg1;
   char *arg2;
   int i, j = 0;
-
+  int fp1, fp2, bytes;
   getArgs(args);
 
   for(i = 0; i < 256; i++) {
     switch (j) {
     case 0:
-      if (args[i] == ' ' && args[i + 1] == '/')
+      if (args[i] == ' ' && args[i + 1] == '/') {
         j = 1;
+        arg1 = &args[i + 1];
+        args[i] = '\0'
+      }
       break;
     case 1:
-      if (args[i] == ' ' && args[i + 1] == '/')
+      if (args[i] == ' ' && args[i + 1] == '/') {
         j = 2;
+        arg2 = &args[i + 1];
+        args[i] = '\0'
+      }
       break;
     case 2:
-      break;
+      if (args[i] == ' ') {
+        args[i] = '\0';
+      }
     }
   }
-
   if(j != 2) {
     puts("Please specify the source file and the dest file.\n\r\0");
+    return;
   }
+  fp1 = fopen(arg1, 'r');
+  fp2 = fopen(arg2, 'w');
+
+  if(fp1 < 0) {
+    puts("Unable to open: ");
+    puts(arg1);
+    puts("\n\r\0");
+    return;
+  }
+  if (fp2 < 0) {
+    puts("Unable to open: ");
+    puts(arg2);
+    puts("\n\r\0");
+    return;
+  }
+
+  bytes = 1024;
+  while(bytes == 1024) {
+    bytes = fread(fp1, buffer, bytes);
+    fwrite(fp2, buffer, bytes);
+  }
+
+}
+
